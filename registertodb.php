57d909e64f4a694414ad5cb19c79297a3c447def
Bleyday <46052824+Bleyday@users.noreply.github.com>
2019-01-09 17:17:42 +0100

    Es wird jetzt für jeden Benutzer eine Datenbank erstellt
    Plan für morgen:
    Inhalte aus der Datenbank auf der Seite des Benutzers anzeigen.

--- registertodb.php
+++ registertodb.php
@@ -1,21 +1,44 @@
+<html lang="de">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="style.css">
+    <title>TAP-Manager</title>
+  </head>
+  <body>
+<h1 id="sitetitle">TAP-Manager</h1>
 <?php 
 $user = $_POST["benutzername"];
 $pass = $_POST["passwort"];
 
 $host = 'localhost';
-$createQ = "CREATE USER '{$user}'@'{$host}' IDENTIFIED BY '{$pass}'"; //In einer Variable gespeichertes                                                                           MYSQL Befehl
+$createU = "CREATE USER '{$user}'@'{$host}' IDENTIFIED BY '{$pass}'"; //In einer Variable gespeichertes                                                                           MYSQL Befehl
+$createDB = "CREATE DATABASE $user";
+$createTB = "CREATE TABLE $user.anmeldedaten(Benutzername VARCHAR(255), Passwort VARCHAR(255))";
+
 $dbuser = "root";
 $dbpass = "Haus2500";
 $dbhost ="localhost";
 $dbname ="test";
 $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname); // Verbindung zur datenbank
-if(mysqli_query($conn, $createQ)){
-	echo 'user created<br/>';
-	if(mysqli_query("GRANT ALL PRIVILEGES ON test.anmeldedaten to '{$user}'@''{$host}'")){ //Rechte für                                                                                            den neuen                                                                                               Benutzer
-		echo 'permissions granted';
-	}else{
-		echo 'permissions query failed';
-	}	
-}else{
-	echo 'user create query failed:';
+
+if(mysqli_query($conn, $createDB)){
+    echo "Datenbank für $user erstellt<br>";
+    $dbname = $user;
 }
+mysqli_query($conn,$createTB);
+if(mysqli_query($conn, $createU)){
+	echo 'Benutzer wurde erstellt';
+    // Rechte für den neuen Benutzer  
+    mysqli_query($conn,"GRANT ALL PRIVILEGES ON $user.anmeldedaten to '$user'@''$host'");
+}
+else{
+	echo "Benutzer konnte nicht erstellt werden<br>" ;
+    }
+?>
+<form action="index.html">
+    <input id = "submit" type="submit" value="Back">
+</form>   
+    </body>
+    
+</html>
