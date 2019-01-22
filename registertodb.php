d18e2fdf2167604eefb5f65f838f65f59b4e013f
Bleyday <46052824+Bleyday@users.noreply.github.com>
2019-01-22 13:15:45 +0100

    .

--- registertodb.php
+++ registertodb.php
@@ -1,12 +1,3 @@
-<html lang="de">
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="style.css">
-    <title>TAP-Manager</title>
-  </head>
-  <body>
-<h1 id="sitetitle">TAP-Manager</h1>
 <?php 
 $user = $_POST["benutzername"];
 $pass = $_POST["passwort"];
@@ -19,7 +10,7 @@ $createTB = "CREATE TABLE $user.anmeldedaten(Benutzername VARCHAR(255), Passwort
 $dbuser = "root";
 $dbpass = "Haus2500";
 $dbhost ="localhost";
-$dbname ="test";
+$dbname ="mysql";
 $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname); // Verbindung zur datenbank
 
 if(mysqli_query($conn, $createDB)){
@@ -35,7 +26,17 @@ if(mysqli_query($conn, $createU)){
 else{
 	echo "Benutzer konnte nicht erstellt werden<br>" ;
     }
+//Hier endet der PHP Teil
 ?>
+<html lang="de">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="style.css">
+    <title>TAP-Manager</title>
+  </head>
+  <body>
+<h1 id="sitetitle"><a href="index.html">TAP-Manager</a></h1>
 <form action="index.html">
     <input id = "submit" type="submit" value="Back">
 </form>   
